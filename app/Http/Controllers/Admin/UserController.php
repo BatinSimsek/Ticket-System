35c5c7cedbeed5e8ad734f79005711173ea1a1d2
Batin <d201115@edu.rocwb.nl>
2020-11-29 21:09:26 +0100

    Admin can change user information

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -83,6 +83,9 @@ class UserController extends Controller
     public function update(Request $request, User $user)
     {
         $user->roles()->sync($request->roles);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
 
         return redirect()->route('admin.user.index');
     }
--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -8,18 +8,46 @@
             <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet aperiam blanditiis consequatur dolore id iste itaque nisi officia reiciendis! Distinctio dolor dolore doloremque esse impedit, iusto nostrum quidem vitae?</p>
 
             <form action="{{ route('admin.user.update', $user) }}" method="post">
-                    @method('PUT')
-                    @csrf
+                @method('PUT')
+                @csrf
 
+                <div class="form-group row">
+                    <label for="email" class="col-md-2 col-form-label text-md-right">Email</label>
 
-                    @foreach($roles as $role)
-                        <input type="checkbox" name="roles[]" value="{{$role->id}}">
-                        <label>{{ $role->name }}</label>
-                    @endforeach
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email" autofocus>
+
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="name" class="col-md-2 col-form-label text-md-right">Name</label>
+
+                    <div class="col-md-6">
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $user->name }}" required autofocus>
+
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                        @enderror
+                    </div>
+                </div>
+
+                @foreach($roles as $role)
+                    <input type="checkbox" name="roles[]" value="{{$role->id}}" @if($user->roles->pluck('id')->contains($role->id)) checked @endif>
+                    <label>{{ $role->name }}</label>
+                @endforeach
 
                 <button type="submit" class="btn btn-primary">
                     Update
                 </button>
+
            </form>
 
         </div>
